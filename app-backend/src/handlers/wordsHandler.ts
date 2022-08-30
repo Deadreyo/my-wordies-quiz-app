a5108887b47782b3cfc522516014b209862b3580
NightKnight <Ahmedatwa866@yahoo.com>
2022-08-30 21:57:03 +0200

    added complex behavior for getWordList

--- app-backend/src/handlers/wordsHandler.ts
+++ app-backend/src/handlers/wordsHandler.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import WordsStore from "../models/wordsModel";
+import getRandomWords from "../services/getRandomWords";
 
 const store = new WordsStore()
 export function getWordsList(req: Request, res: Response) {
     try {
-        const list = store.getWordsList()
+        const list = getRandomWords()
         res.status(200).json(list)
     } catch (e) {
         res.status(500).send('Internal Server Error. Try again.')
---
+++ app-backend/src/services/getRandomWords.ts
@@ -0,0 +1,26 @@
+import WordsStore, { WordItem } from "../models/wordsModel";
+import shuffleArray from "./helpers/shuffleArray";
+
+const store = new WordsStore()
+export default function getRandomWords(): WordItem[] {
+    // get the data from store
+    let data = store.getWordsList()
+
+    // make sure there's 1 of each type (i.e. add 1 of each type)
+    let newData: WordItem[] = [];
+    ['noun', 'verb', 'adverb', 'adjective'].forEach(pos => {
+        let found = data.find( val => val.pos === pos)!
+        newData.push(found)
+        
+        // remove that element so there's no duplicate elements in the array
+        data.splice( data.indexOf(found), 1 )
+    });
+    
+    // then add more elements, and take only 10
+    newData.push(...data)
+    newData.length = 10;
+
+    // shuffle the data
+    let shuffledData = shuffleArray(newData)
+    return shuffledData
+}
