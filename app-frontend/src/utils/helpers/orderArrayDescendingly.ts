2a0037d7f097f0fdb729612f3f7b248b613323ac
NightKnight <Ahmedatwa866@yahoo.com>
2022-08-30 21:32:50 +0200

    remove console log

--- app-frontend/src/utils/helpers/orderArrayDescendingly.ts
+++ app-frontend/src/utils/helpers/orderArrayDescendingly.ts
@@ -1,6 +1,5 @@
 
 export default function orderArrayDescendingly(array: number[]): number[] {
     const newArray = array.sort( (a, b) => b - a)
-    console.log('new array: ', newArray)
     return newArray
 }
